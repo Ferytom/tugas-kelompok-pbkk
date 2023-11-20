5592e8ea4ab072bfc6ba8ceb03e13f22129f833f
RichardRyan141 <114993335+RichardRyan141@users.noreply.github.com>
2023-11-20 09:57:45 +0700

    daily reports

--- resources/views/report/daily.blade.php
+++ resources/views/report/daily.blade.php
@@ -1,7 +1,28 @@
 @extends('master')
-@section('content')
-    <h1>Hello, this is Daily Report List</h1>
 
-    <a href="{{ route('report.index') }}" class="btn-info mr-2" style="padding-top:10px">Report List</a>
+@section('content')
+    <h1>Daily Report</h1>
+    <table class="data-table">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Total Income</th>
+            </tr>
+        </thead>
+        <tbody id="data-table-body">
+            @forelse($groupedTransactions as $date => $transactions)
+                    <tr>
+                        <td>{{ Carbon\Carbon::parse($date)->format('F j, Y') }}</td>
+                        <td>Rp {{ number_format($dailyTotals[$date], 0) }}</td>
+                    </tr>
+            @empty
+                    <tr>
+                        <td colspan="2">No transactions available.</td>
+                    </tr>
+            @endforelse
+        </tbody>
+    </table>
+    <a href="{{ route('report.index') }}" class="btn-info mr-2" style="padding-top:10px">Full Report List</a>
     <a href="{{ route('report.monthly') }}" class="btn-info mr-2" style="padding-top:10px">Monthly Report List</a>
+
 @endsection
