819b8406b5357bdacd8a2c8171728f49e8e93b24
RichardRyan141 <114993335+RichardRyan141@users.noreply.github.com>
2023-12-04 18:14:23 +0700

    update sidebar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -277,7 +277,7 @@
                             <span>Locations</span>
                         </div>
                         <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ExpandLessIcon" height="1.5rem">
-                            <path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z" id="arrowPathTransaction"></path>
+                            <path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z" id="arrowPathLocation"></path>
                         </svg>
                         <span></span>
                     </div>
